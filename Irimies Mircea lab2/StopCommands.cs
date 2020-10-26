4c2cf01833edf334a1df942f8f156ab6f41f4042
Mircea Irimies <mirceairimies33@gmail.com>
2020-10-26 20:10:19 +0200

    dada merge ca uns

---
+++ Irimies Mircea lab2/StopCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+namespace Irimies_Mircea_lab2.CustomCommands
+{
+    class StopCommands
+    {
+        private static RoutedUICommand launch_command;
+        static StopCommands()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            launch_command = new RoutedUICommand("Launch", "Launch",
+           typeof(StopCommands), myInputGestures);
+        }
+        public static RoutedUICommand Launch
+        {
+            get
+            {
+                return launch_command;
+            }
+        }
+    }
+}
